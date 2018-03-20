bf9f2becfd5266f7c36b10962d26c9267da19183
Joel Rorseth <joelrorseth@gmail.com>
2018-03-20 17:37:29 -0400

    Get Overview screen working with Adapters for the info

--- app/src/main/java/com/rorsethj/expertexpense/OverviewFragment.java
+++ app/src/main/java/com/rorsethj/expertexpense/OverviewFragment.java
@@ -3,12 +3,23 @@ package com.rorsethj.expertexpense;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-public class OverviewFragment extends Fragment {
+public class OverviewFragment extends Fragment
+        implements MyAccountsRecyclerAdapter.ItemClickListener,
+        RecentTransactionsRecyclerAdapter.ItemClickListener {
+
+
+    private MyAccountsRecyclerAdapter myAccountsAdapter;
+    private RecentTransactionsRecyclerAdapter recentTransactionsAdapter;
 
 
     @Override
@@ -18,6 +29,44 @@ public class OverviewFragment extends Fragment {
 
         // Inflate XML resource for this fragment
         View view = inflater.inflate(R.layout.fragment_overview, container, false);
+
+
+        // TODO, just temporary
+        // Populate data for recycler
+        ArrayList<String> tempAccounts = new ArrayList<>();
+        tempAccounts.add("Cash");
+        tempAccounts.add("Bank");
+        tempAccounts.add("Offshore");
+
+        ArrayList<String> tempTrans = new ArrayList<>();
+        tempTrans.add("Groceries");
+        tempTrans.add("Hydro Bill");
+
+
+        // Set up recycler views
+        RecyclerView accountsRecyclerView = (RecyclerView) view.findViewById(R.id.myAccountsRecycler);
+        accountsRecyclerView.setLayoutManager(new LinearLayoutManager(
+                getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+
+        RecyclerView transRecyclerView = (RecyclerView) view.findViewById(R.id.recentTransactionsRecycler);
+        transRecyclerView.setLayoutManager(new LinearLayoutManager(
+                getContext(), LinearLayoutManager.VERTICAL, false));
+
+
+        myAccountsAdapter = new MyAccountsRecyclerAdapter(getContext(), tempAccounts);
+        recentTransactionsAdapter = new RecentTransactionsRecyclerAdapter(getContext(), tempTrans);
+
+        myAccountsAdapter.setClickListener(this);
+        recentTransactionsAdapter.setClickListener(this);
+
+        // Set adapters to recycler views
+        accountsRecyclerView.setAdapter(myAccountsAdapter);
+        transRecyclerView.setAdapter(recentTransactionsAdapter);
+
+
+
+
 
         // Configure floating action button listener
         FloatingActionButton addButton = (FloatingActionButton) view.findViewById(R.id.addButton);
@@ -33,5 +82,14 @@ public class OverviewFragment extends Fragment {
         );
 
         return view;
+    }
+
+
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getContext(), "You clicked " +
+                myAccountsAdapter.getItem(position) + " on item position " +
+                position, Toast.LENGTH_SHORT).show();
     }
 }
