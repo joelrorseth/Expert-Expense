c6ed0367bc423f2d9eeeb829ddb869fa2a85bad3
Joel Rorseth <joelrorseth@gmail.com>
2018-03-25 23:22:38 -0400

    Setup General Popup with handlers for buttons

--- app/src/main/java/com/rorsethj/expertexpense/GeneralPopupFragment.java
+++ app/src/main/java/com/rorsethj/expertexpense/GeneralPopupFragment.java
@@ -6,8 +6,18 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class GeneralPopupFragment extends DialogFragment {
+
+    private final int popupButtonIDs[] = {
+            R.id.genPopupTransactions,
+            R.id.genPopupEditAcct,
+            R.id.genPopupAddTrans,
+            R.id.genPopupTransfer,
+            R.id.genPopupDelete,
+            R.id.genPopupHide };
+
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -23,7 +33,59 @@ public class GeneralPopupFragment extends DialogFragment {
         // Set the dialog layout
         builder.setView(view);
 
+
+        // Setup a listener for each popup button
+        for (int id: popupButtonIDs) {
+            final int _id = id;
+
+            ((ImageButton) view.findViewById(id)).setOnClickListener(new View.OnClickListener() {
+
+                // Pass of handling to handleClickFor to determine appropriate course of action
+                @Override
+                public void onClick(View view) {
+                    handleClickFor(_id);
+                }
+            });
+        }
+
         return builder.create();
     }
 
+
+    // Handler for click events for a given button with ID
+    private void handleClickFor(int buttonID) {
+
+        switch (buttonID) {
+
+            case R.id.genPopupTransactions:
+
+                // Go to TransactionFragment, displaying transactions for this account only
+                break;
+
+            case R.id.genPopupEditAcct:
+
+                // Go to edit account screen
+                break;
+
+            case R.id.genPopupAddTrans:
+
+                // Go to add transaction screen, with calculator prompted
+                break;
+
+            case R.id.genPopupTransfer:
+
+                // Go to transfer creation screen
+                break;
+
+            case R.id.genPopupDelete:
+
+                // Confirm the deletion, if confirmed, delete account from records
+                break;
+
+            case R.id.genPopupHide:
+
+                // Hide this account from views
+                break;
+        }
+    }
 }
